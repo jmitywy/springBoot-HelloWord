bda722e1e88f2f803885965b95aaf312a3698bf4
yydsj <1612960620@qq.com>
2022-03-06 22:50:54 +0800

    第二次提交

--- src/main/java/com/yang/MainApplication.java
+++ src/main/java/com/yang/MainApplication.java
@@ -1,9 +1,7 @@
 package com.yang;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import java.util.HashMap;
